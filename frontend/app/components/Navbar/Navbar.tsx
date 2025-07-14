5c3dd5be6f5ec035177300235db5e35ef913f8fc
Vineet Jadhav <vineet290403@gmail.com>
2025-07-14 20:42:28 +0530

    Fix: Navbar Responsive

--- frontend/app/components/Navbar/Navbar.tsx
+++ frontend/app/components/Navbar/Navbar.tsx
@@ -10,9 +10,9 @@ export default function Navbar() {
             initial={{ opacity: 0, y: 50 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.8, delay: 0.3 }}
-            className="sticky top-0 z-50 backdrop-blur-2xl bg-white/10 shadow-2xl border border-white/20 rounded-3xl px-6 py-5 mx-auto w-[95%] max-w-6xl flex items-center justify-between glassmorphic text-white/80 font-inter">
+            className="sticky top-0 z-50 backdrop-blur-2xl bg-white/10 shadow-2xl border border-white/20 rounded-3xl px-6 py-5 mx-auto w-[100%] max-w-7xl flex items-center justify-between glassmorphic text-white/80 font-inter">
             {/* Logo */}
-            <div className="text-2xl font-extrabold text-white instrument-serif-regular-italic">
+            <div className="text-xl lg:text-2xl font-extrabold text-white instrument-serif-regular-italic">
                 Screenshot <span className="text-blue-400">AI</span>
             </div>
 
@@ -24,13 +24,13 @@ export default function Navbar() {
             </div>
 
             {/* Auth Buttons */}
-            <div className="space-x-4 font-medium">
+            <div className=" flex gap-4 justify-center items-center text-center font-medium">
                 <Link href="/login" className="text-white hover:text-blue-300 cursor-pointer transition text-sm">
                     Login
                 </Link>
 
-                <Link href="/signup" className="bg-blue-500 hover:bg-blue-600 text-white cursor-pointer px-4 py-1.5 rounded-full text-sm transition">
-                    Sign Up
+                <Link href="/signup" className="bg-blue-500 hover:bg-blue-600 text-white cursor-pointer px-2 py-1.5 rounded-full text-sm transition text-center">
+                    Signup
                 </Link>
             </div>
         </motion.nav>
